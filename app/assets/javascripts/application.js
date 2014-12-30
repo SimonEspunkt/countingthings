244cb4c881045714d46e9112a4cd45b16c4277fb
SimonZero <simon.streubel@gmail.com>
2014-12-31 00:20:21 +0100

    included page specific jquery.ready selector

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,8 @@
 //= require jquery_ujs
 //= require foundation
 //= require turbolinks
+//= require jquery-readyselector
+//= require Chart
 //= require_tree .
 
 $(function(){ $(document).foundation(); });
